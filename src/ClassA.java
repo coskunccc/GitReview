cccc5e4734cf09089dbf3688ab8e596c2d8775de
coskunccc <coskunarslan@yahoo.com>
2022-09-05 20:45:10 +0300

    Branch changed classA

--- src/ClassA.java
+++ src/ClassA.java
@@ -2,6 +2,7 @@ public class ClassA {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("tracking started");
+        //Branch2 made change
 
     }
 }
